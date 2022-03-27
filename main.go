7f14566eacdc1c4ab46f623546ba462fa7852d27
Hossein Hazratgholizadeh <hossein.hazratgholizade@gmail.com>
2022-03-28 02:21:39 +0300

    Create struct and connection with db

--- main.go
+++ main.go
@@ -1,27 +1,58 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/jinzhu/gorm"
 )
 
-type Person struct{
+type Person struct {
 	// next line is create unique id, date created and date updated and date deleted
 	gorm.Model
-	Name string
+	Name  string
 	Email string `gorm:"typevarchar(100);unique_index"` // with help of gorm we make email a unique value
-	Book []Book
+	Book  []Book
 }
 
 type Book struct {
 	gorm.Model
 
-	Title string
-	Author string
+	Title      string
+	Author     string
 	CallNumber int
-	PersonID int // use name of the struct that you want make relationship
+	PersonID   int // use name of the struct that you want make relationship
 
 }
 
-func main(){
-	
+var db *gorm.DB
+var err error
+
+func main() {
+	// LOading enviromental variables
+	dialect := os.Getenv("DIALECT")
+	host := os.Getenv("HOST")
+	dbPort := os.Getenv("DBPORT")
+	user := os.Getenv("USER")
+	dbName := os.Getenv("NAME")
+	password := os.Getenv("PASSWORD")
+
+	// Database connection string
+	dbURI := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s port=%s", host, user, dbName, password, dbPort)
+
+	// Openning connection with database
+	db, err = gorm.Open(dialect, dbURI)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("Successfully connected to database!")
+	}
+
+	// close connection to database when the main function finishes
+	defer db.Close()
+
+	// Make migrations to database if they have not already created
+	db.AutoMigrate(&Person{})
+	db.AutoMigrate(&Book{})
 }
