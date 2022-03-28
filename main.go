08638bec1ac600e1309f24353e97d13206ea82fe
Hossein Hazratgholizadeh <hossein.hazratgholizade@gmail.com>
2022-03-28 05:04:45 +0300

    Connection Is OK
    connection with postgres database is done

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
@@ -26,11 +27,24 @@ type Book struct {
 
 }
 
+var (
+	persons = []Person{
+		{Name: "Hossein", Email: "hossein@gmail.com"},
+		{Name: "Lia", Email: "lia1989@gmail.com"},
+	}
+	books = []Book{
+		{Title: "Lord of Glory", Author: "Benjamin B. Warfield", CallNumber: 238268289, PersonID: 1},
+		{Title: "The Lord of Death and the Queen of Life", Author: "Homer Eon Flint", CallNumber: 234568288, PersonID: 1},
+		{Title: "The Best Horror of the Year: Volume 1", Author: "Ellen Datlow", CallNumber: 762365490, PersonID: 2},
+		{Title: "Finnish the Language", Author: "Unknown", CallNumber: 1236176, PersonID: 2},
+	}
+)
+
 var db *gorm.DB
 var err error
 
 func main() {
-	// LOading enviromental variables
+	// // Loading enviromental variables - .env file is not working
 	// dialect := os.Getenv("DIALECT")
 	// host := os.Getenv("HOST")
 	// dbPort := os.Getenv("DBPORT")
@@ -61,4 +75,19 @@ func main() {
 	// Make migrations to database if they have not already created
 	db.AutoMigrate(&Person{})
 	db.AutoMigrate(&Book{})
+
+	// // creating some dummy data for testing connection with db
+	// for idx := range persons {
+	// 	db.Create(&persons[idx])
+	// }
+
+	// for idx := range books {
+	// 	db.Create(&books[idx])
+	// }
+
+
+	// API routers
+	router := mux.NewRouter()
+	router.HandleFunc("/people", getPeople).Methods("GET")
+
 }
