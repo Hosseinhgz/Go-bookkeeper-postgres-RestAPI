667abba5eadc2749000950246565bfd8ef1da469
Hossein Hazratgholizadeh <hossein.hazratgholizade@gmail.com>
2022-04-04 16:26:35 +0300

    Update app.go

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -25,12 +25,18 @@ func GetDB() *gorm.DB {
 		fmt.Println(".env file is succesfully loaded")
 	}
 
-	dialect := os.Getenv("DIALECT")
-	host := os.Getenv("HOST")
-	dbPort := os.Getenv("DB_PORT")
-	user := os.Getenv("USER")
-	dbName := os.Getenv("DB_NAME")
-	password := os.Getenv("PASS")
+	// dialect := os.Getenv("DIALECT")
+	// host := os.Getenv("HOST")
+	// dbPort := os.Getenv("DB_PORT")
+	// user := os.Getenv("USER")
+	// dbName := os.Getenv("DB_NAME")
+	// password := os.Getenv("PASS")
+	dialect := "postgres"
+	host := "ec2-52-30-67-143.eu-west-1.compute.amazonaws.com"
+	dbPort := "5432"
+	user := "fepuzbklejrydh"
+	dbName := "d1a6j2p9b55cbk"
+	password := "2b5141aad07ec369afa35e1e173ab964529665dab2b4f31afb2e10a2ea593c5f"
 
 	// Database connection string
 	dbURI := fmt.Sprintf("host=%s user=%s dbname=%s password=%s port=%s", host, user, dbName, password, dbPort)
