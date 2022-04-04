aff737a47d38819df58bc1c07ac38f0c08380efd
Hossein Hazratgholizadeh <hossein.hazratgholizade@gmail.com>
2022-04-04 14:42:47 +0300

    Update app.go

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -16,21 +16,20 @@ var db *gorm.DB
 var err error
 
 func GetDB() *gorm.DB {
-	envErr := godotenv.Load("../../.env")
+	envErr := godotenv.Load("github.com/Hosseinhgz/Go-bookkeeper-postgres-RestAPI/.env")
 	if envErr != nil {
 		fmt.Printf("Could not load the .env file")
 		os.Exit(1)
-	}else{
+	} else {
 		fmt.Printf(".env file is succesfully loaded")
 	}
-	dialect := os.Getenv("DIALECT")
 
-	// deployed version Heroku
-	host := "ec2-52-30-67-143.eu-west-1.compute.amazonaws.com"
-	dbPort := "5432"
-	user := "fepuzbklejrydh"
-	dbName := "d1a6j2p9b55cbk"
-	password := "2b5141aad07ec369afa35e1e173ab964529665dab2b4f31afb2e10a2ea593c5f"
+	dialect := os.Getenv("DIALECT")
+	host := os.Getenv("HOST")
+	dbPort := os.Getenv("DB_PORT")
+	user := os.Getenv("USER")
+	dbName := os.Getenv("DB_NAME")
+	password := os.Getenv("PASS")
 
 	// Database connection string
 	dbURI := fmt.Sprintf("host=%s user=%s dbname=%s password=%s port=%s", host, user, dbName, password, dbPort)
