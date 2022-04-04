231d7074c4bcbd29889cd6ccc8a1e1d01c5dd993
Hossein Hazratgholizadeh <hossein.hazratgholizade@gmail.com>
2022-04-04 14:59:29 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,26 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/Hosseinhgz/Go-bookkeeper-postgres-RestAPI/pkg/routes"
 	"github.com/gorilla/mux"
-)
-
-var (
-	persons = []models.Person{
-		{Name: "Hossein", Email: "hossein@gmail.com"},
-		{Name: "Lia", Email: "lia1989@gmail.com"},
-	}
-	books = []models.Book{
-		{Title: "Lord of Glory", Author: "Benjamin B. Warfield", CallNumber: 238268289, PersonID: 1},
-		{Title: "The Lord of Death and the Queen of Life", Author: "Homer Eon Flint", CallNumber: 234568288, PersonID: 1},
-		{Title: "The Best Horror of the Year: Volume 1", Author: "Ellen Datlow", CallNumber: 762365490, PersonID: 2},
-		{Title: "Finnish the Language", Author: "Unknown", CallNumber: 1236176, PersonID: 2},
-	}
 )
 
 func main() {
