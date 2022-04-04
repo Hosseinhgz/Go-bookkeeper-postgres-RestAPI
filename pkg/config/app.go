bdb8776c8680cc8a53f1785f20b10fbebf8a8c94
Hossein Hazratgholizadeh <hossein.hazratgholizade@gmail.com>
2022-04-04 15:32:01 +0300

    Update app.go

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -3,10 +3,8 @@ package config
 import (
 	"fmt"
 	"log"
-	"os"
 
 	"github.com/Hosseinhgz/Go-bookkeeper-postgres-RestAPI/pkg/models"
-	"github.com/joho/godotenv"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
@@ -17,13 +15,13 @@ var err error
 
 func GetDB() *gorm.DB {
 
-	envErr := godotenv.Load()
-	if envErr != nil {
-		fmt.Println("Could not load the .env file")
-		os.Exit(1)
-	} else {
-		fmt.Println(".env file is succesfully loaded")
-	}
+	// envErr := godotenv.Load()
+	// if envErr != nil {
+	// 	fmt.Println("Could not load the .env file")
+	// 	os.Exit(1)
+	// } else {
+	// 	fmt.Println(".env file is succesfully loaded")
+	// }
 
 	// dialect := os.Getenv("DIALECT")
 	// host := os.Getenv("HOST")
